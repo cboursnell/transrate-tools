f50d43b124689091756751a347bdd0cae580a802
Chris Boursnell <cboursnell@gmail.com>
2014-10-27 14:59:52 +0000

    increase bam-split version; new binaries

--- transrate-bam-split.cpp
+++ transrate-bam-split.cpp
@@ -234,7 +234,7 @@ int main (int argc, char* argv[]) {
     }
 
   } else {
-    cout << "bam-split version 0.1\n"
+    cout << "bam-split version 0.2\n"
             "Usage:\n"
             "bam-split <bam_file>" << endl;
     return 1;
