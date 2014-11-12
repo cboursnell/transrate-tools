21ec96e6e99ac1540c18295d611fe8d9cfd74f92
Richard Smith <rds45@cam.ac.uk>
2014-11-12 13:19:45 +0000

    bump patch version

--- transrate-bam-read.cpp
+++ transrate-bam-read.cpp
@@ -236,7 +236,7 @@ int main (int argc, char* argv[]) {
     output.close();
     return 0;
   } else {
-    cout << "bam-read version 0.3.5\n"
+    cout << "bam-read version 0.3.6\n"
          << "Usage:\n"
          << "bam-read <bam_file> <output_csv> <nullprior (optional)>\n\n"
          << "example:\n"
